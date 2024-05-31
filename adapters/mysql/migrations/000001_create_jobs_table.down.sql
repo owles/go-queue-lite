87fd085d584a31ddd6ede3e46630900596628d03
DmVsk <dm.vsk@yandex.ru>
2024-05-31 14:03:50 +0700

    Fix MySQL down

--- adapters/mysql/migrations/000001_create_jobs_table.down.sql
+++ adapters/mysql/migrations/000001_create_jobs_table.down.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS jobs;
 
-DROP INDEX IF EXISTS idx_jobs_queue ON jobs;
-DROP INDEX IF EXISTS idx_jobs_count ON jobs;
-DROP INDEX IF EXISTS idx_jobs_score ON jobs;
-DROP INDEX IF EXISTS idx_jobs_available_at ON jobs;
+DROP INDEX idx_jobs_queue ON jobs;
+DROP INDEX idx_jobs_count ON jobs;
+DROP INDEX idx_jobs_score ON jobs;
+DROP INDEX idx_jobs_available_at ON jobs;
